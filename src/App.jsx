8ba13eff2c37b7fb156aef5da67aa0f3f61df4ab
SayedMohebSadat460 <sadatsm111@gmail.com>
2024-10-02 10:52:30 +0430

    changed

--- src/App.jsx
+++ src/App.jsx
@@ -24,7 +24,7 @@ const App = () => {
         </Routes>
         <div>
       <Button text="Submit" onClick={handleClick} />
-      <Button text="Cancel" onClick={() => alert('Canceled!')}  />
+      <Button text="Cancel" onClick={() => alert('Canceled')}  />
     </div>
       </div>
     </Router>
